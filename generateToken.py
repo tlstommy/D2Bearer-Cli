8423f31cbabdf5e1a037868c47534ff13be06b72
lulamae12 <tls.tommy11@gmail.com>
2023-01-09 14:09:04 -0600

    commit

--- generateToken.py
+++ generateToken.py
@@ -12,12 +12,14 @@ def clearConsole():
 
 
 
-
 class main:
     def __init__(self,clientID,clientSecret):
+        self.apiUrl = "https://www.bungie.net/Platform/App/OAuth/Token/"
         self.clientID = clientID
         self.clientSecret = clientSecret
         self.oAuthCode = None
+        self.apiJsonResponse = None
+        self.bearerToken = None
 
     def genAuthURL(self):
         print("\nPlease open this link in a browser and sign in to your Bungie account.")
@@ -28,29 +30,44 @@ class main:
     def processRedirectURL(self,redirectURL):
         self.oAuthCode = redirectURL.split("code=")[-1]
         print(f"\n[OAuth Code] {self.oAuthCode}")
+        self.grabNewBearerToken()
     
     def grabNewBearerToken(self):
         
-        #format the oAuthDataString
-        oAuthDataString = f"client_id={self.clientID}&client_secret={self.clientSecret}&Authorization%3A%20Basic%20%7Bbase64encoded(client-id%3Aclient-secret)%7D=&Content-Type%3A%20application%2Fx-www-form-urlencoded=&grant_type=authorization_code&code="
+        #format the oAuthDataString with client id and secret and append the oauth code
+        oAuthDataString = f"client_id={self.clientID}&client_secret={self.clientSecret}&Authorization%3A%20Basic%20%7Bbase64encoded(client-id%3Aclient-secret)%7D=&Content-Type%3A%20application%2Fx-www-form-urlencoded=&grant_type=authorization_code&code={self.oAuthCode}"
 
+        #setup headers content type
         headers = CaseInsensitiveDict()
         headers["Content-Type"] = "application/x-www-form-urlencoded"
 
+        #send a POST request to the api 
+        resp = requests.post(self.apiUrl, headers=headers, data=oAuthDataString)
 
-        data = OAUTH_DATASTR + OAUTH_CODE
+        #pull out the access token
+        self.apiJsonResponse = jsonResponse = json.loads(resp.content)
+        self.bearerToken = jsonResponse["access_token"]
+        self.tokenOutput()
+    def tokenOutput(self):
+        clearConsole()
+        print(f"[Full API JSON Response]\n{self.apiJsonResponse}\n\n[Bearer Token]\n{self.bearerToken}\n")
+
+        while True:
+            userInput = input("Would you like to save the bearer token to a textfile in the current directory? (Y/n)")
+            if userInput == "Y":
+                self.saveToFile()
+                break
+            elif userInput == "n":
+                break
+            else:
+                print("\nInvalid option! Please enter 'Y' or 'n'\n")
+
+ 
 
 
-        resp = requests.post(url, headers=headers, data=data)
-
-    jsonResponse = json.loads(resp.content)
-    print(jsonResponse["access_token"])
-    return jsonResponse["access_token"]
-
-
-    def saveToFile(self,bearerToken):
+    def saveToFile(self):
         with open('bearer_token.txt', 'w') as f:
-            f.write(bearerToken)
+            f.write(self.bearerToken)
             f.close()
 
 
@@ -63,8 +80,6 @@ cliID = input("Client ID:{:5}".format(" "))
 cliSecret = input("\nClient Secret: ")
 
 clearConsole()
-
-print("[D2Bearer-Cli] \n")
 print("[Client ID]{:7}{cliID}".format(" ",cliID=cliID))
 print("[Client Secret]{:3}{cliSecret}".format(" ",cliSecret=cliSecret))
 
@@ -73,3 +88,5 @@ print("[Client Secret]{:3}{cliSecret}".format(" ",cliSecret=cliSecret))
 Generator = main(cliID,cliSecret)
 
 Generator.genAuthURL()
+
+print("Thank you for using D2Bearer-Cli!")
